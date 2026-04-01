baeb9af6d64d034632a7cfb2e13cb3b793f60ff0
Rodrigo Martinez <rodrigo.nmartinez@gmail.com>
2026-04-01 19:04:15 -0300

    Operadores

---
+++ formatear_cadenas.py
@@ -0,0 +1,4 @@
+color= "gris"
+matricula= "INU641"
+
+print(f"Mi auto es de color {color} y su matricula es {matricula}")
---
+++ operadores.py
@@ -0,0 +1,15 @@
+x= 6
+y= 2
+z= 7
+
+print(f"{x} mas {y} es igual a {x+y}")  
+print(f"{x} menos {y} es igual a {x-y}")  
+print(f"{x} por {y} es igual a {x*y}")  
+print(f"{x} entre {y} es igual a {x/y}")  
+print(f"{z} entre {y} es igual a {z//y}")  
+print(f"{z} modulo {y} es igual a {z%y}")  
+print(f"{x} elevado a {y} es igual a {x**y}")  
+
+
+print(f"{z} dividido al piso por {y} es igual a {z//y}")
+print(f"La raiz cuadrada de {z} es igual a {z**0.5} ")
