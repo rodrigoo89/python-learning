473094c74d4dce720b40072a15c7cf062f3f246c
Rodrigo Martinez <rodrigo.nmartinez@gmail.com>
2026-04-02 01:45:25 -0300

    Dia 3

---
+++ dia2/formatear_cadenas.py
@@ -0,0 +1,6 @@
+color= "gris"
+matricula= "INU641"
+
+print(f"Mi auto es de color {color} y su matricula es {matricula}")
+
+print("Mi auto es de color " + color + " y su matricula es " + matricula)
---
+++ dia3/index.py
@@ -0,0 +1,4 @@
+mi_texto = "Esta es una prueba de formato de texto"
+
+resultado= mi_texto.index("n")
+print(resultado)
